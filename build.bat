c0bde197cc73d0d3472a5ceb211d05efe500a168
xyrox <87136072+xyrox924@users.noreply.github.com>
2026-06-06 02:09:58 +0300

    remove build.bat

--- build.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --paths=src --add-data "src/recap_reloaded/gui/icons;recap_reloaded/gui/icons" --icon="src/recap_reloaded/gui/icons/favicon.ico" --name="recap reloaded" --windowed main.py
